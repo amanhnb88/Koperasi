cf684c474ab9f5abf7437ef8acfa648c4dd8b0d1
amanhnb88 <aldry.manu02@gmail.com>
2026-04-14 02:00:18 +0900

    Refactor comments in MainActivity.kt
    Removed introductory comments and updated code comment.

--- app/src/main/java/com/amanhnb88/koperasi/MainActivity.kt
+++ app/src/main/java/com/amanhnb88/koperasi/MainActivity.kt
@@ -3,13 +3,10 @@ package com.amanhnb88.koperasi
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-/**
- * Ini adalah pintu masuk atau halaman pertama yang akan dimuat
- * saat aplikasi KSP Semy Riry dibuka.
- */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Nantinya, kode untuk memanggil tampilan UI Dark Mode akan kita taruh di sini
+        // Baris ini menyambungkan kode Kotlin dengan desain XML yang kita buat
+        setContentView(R.layout.activity_main)
     }
 }
