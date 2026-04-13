d73c8adf5269fb4cc6838605ee1a53c51ad577db
amanhnb88 <aldry.manu02@gmail.com>
2026-04-14 01:48:27 +0900

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// File ini mengatur versi plugin dasar untuk Android dan Kotlin
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
