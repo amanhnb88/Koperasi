acd0c18ff30d79d7786b7c4366a04136b5d5d382
amanhnb88 <aldry.manu02@gmail.com>
2026-04-14 01:51:01 +0900

    Create MainActivity.kt

---
+++ app/src/main/java/com/amanhnb88/koperasi/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.amanhnb88.koperasi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Ini adalah pintu masuk atau halaman pertama yang akan dimuat
+ * saat aplikasi KSP Semy Riry dibuka.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Nantinya, kode untuk memanggil tampilan UI Dark Mode akan kita taruh di sini
+    }
+}
